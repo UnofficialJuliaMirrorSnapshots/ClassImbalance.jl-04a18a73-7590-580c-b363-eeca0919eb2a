cf4b6e77170278ecfcf53520800d3f45b492b127
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-04-07 19:47:55 +0000

    Snapshot of branch master taken on 2019-04-07T15:47:54.524-04:00 from "https://github.com/UnofficialJuliaMirror/ClassImbalance.jl-04a18a73-7590-580c-b363-eeca0919eb2a"

---
+++ ci/travis/script.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+##### Beginning of file
+
+set -ev
+
+# Remove the following line after we add the Project.toml file
+julia --check-bounds=yes --color=yes -e 'import Pkg; Pkg.clone(pwd());'
+
+julia --check-bounds=yes --color=yes -e '
+    import Pkg;
+    Pkg.build("ClassImbalance");
+    '
+
+julia --check-bounds=yes --color=yes -e '
+    import ClassImbalance;
+    '
+
+julia --check-bounds=yes --color=yes -e '
+    import Pkg;
+    Pkg.test("ClassImbalance"; coverage=true);
+    '
+
+julia --check-bounds=yes --color=yes -e '
+    import Pkg;
+    try Pkg.add("Coverage") catch end;
+    '
+
+julia --check-bounds=yes --color=yes -e '
+    import Coverage;
+    import ClassImbalance;
+    cd(normpath(joinpath(pathof(ClassImbalance), "..", "..")));
+    Coverage.Codecov.submit(Coverage.Codecov.process_folder());
+    '
+
+##### End of file
